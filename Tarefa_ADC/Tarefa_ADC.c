86dbbdcb0b2dcf57243dae724be21869749aabd6
PauloCesar53 <pcdilauro53@gmail.com>
2025-02-17 18:35:40 -0300

    Add:Bibliotecas para display. Alteração CMake e testes display

--- Tarefa_ADC/Tarefa_ADC.c
+++ Tarefa_ADC/Tarefa_ADC.c
@@ -3,6 +3,18 @@
 #include "hardware/adc.h"//biblioteca para funções  adc
 #include "hardware/pwm.h"//biblioteca para funções  pwm
 #include "hardware/clocks.h"
+//bibliotecas para display 
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+//definições para display 
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+//Trecho para modo BOOTSEL com botão B
+#include "pico/bootrom.h"
+#define botaoB 6
 
 #define JOY_Y 26//eixo  y segundo diagrama BitDogLab
 #define JOY_X 27//eixo  x segundo diagrama BitDogLab
@@ -24,6 +36,10 @@ int main()
     adc_init();//inicializando adc
     adc_gpio_init(JOY_Y);//inicializando pino direção y 
     adc_gpio_init(JOY_X);//inicializando pino direção x
+    // Para ser utilizado o modo BOOTSEL com botão B
+    gpio_init(botaoB);
+    gpio_set_dir(botaoB, GPIO_IN);
+    gpio_pull_up(botaoB);
     //configurnado botão joy com ativação do pull up interno 
     gpio_init(JOY_botton);
     gpio_set_dir(JOY_botton, GPIO_IN);
@@ -46,16 +62,51 @@ int main()
     gpio_set_irq_enabled_with_callback(Botao_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     // configurando a interrupção com botão na descida para o botão JOY
     gpio_set_irq_enabled_with_callback(JOY_botton, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    //testes display
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+    ssd1306_t ssd;                                                // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                         // Configura o display
+    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+    bool cor = true;
     while (true) {
         adc_select_input(0);//canal adc JOY para eixo y
         uint16_t JOY_Y_value = adc_read(); // Lê o valor do eixo y, de 0 a 4095.
         adc_select_input(1);//canal adc JOY para eixo x
         uint16_t JOY_X_value = adc_read();// Lê o valor do eixo x, de 0 a 4095.
         uint  JOY_botton_value= gpio_get(JOY_botton) == 0; // 0 indica que o botão está pressionado.
+    
         if(aux_LED_PWM){
             LED_Control(JOY_Y_value, JOY_X_value, ajuste, pwm_wrap);
         }
+        //testes
+        ssd1306_fill(&ssd, !cor); // Limpa o display
+        // ,   ,    ,direita,down, 
+        ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor); // Desenha um retângulo
+        //ssd1306_rect(&ssd, 4, 4, 121, 59, cor, !cor); // Desenha um retângulo
+        //ssd1306_rect(&ssd, 5, 5, 120, 57, cor, !cor); // Desenha um retângulo
+       // ssd1306_line(&ssd, 3, 37, 123, 37, cor); // Desenha uma linha   
+        //ssd1306_line(&ssd, 44, 37, 44, 60, cor); // Desenha uma linha vertical         
+        //ssd1306_line(&ssd, 84, 37, 84, 60, cor); // Desenha uma linha vertical       
+        //ssd1306_rect(&ssd, 52, 90, 8, 8, cor,!gpio_get(JOY_botton)); // Desenha um retângulo  
+       // ssd1306_rect(&ssd, 52, 102, 8, 8, cor, !gpio_get(Botao_A)); // Desenha um retângulo    
+        //ssd1306_rect(&ssd, 52, 114, 8, 8, cor, !cor); // Desenha um retângulo       
+        ssd1306_send_data(&ssd); // Atualiza o display
+        //testes 
         sleep_ms(100);
+        if(gpio_get(botaoB)==0){
+            reset_usb_boot(0, 0);
+        }
     }
     return 0;
 }
